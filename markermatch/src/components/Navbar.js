3721c375e2e0cc77fb3a3cbf4ccbf4bf1620091e
Saif3n <nicholaswu498@gmail.com>
2023-09-03 23:17:51 +1200

    created navigation bar

---
+++ markermatch/src/components/Navbar.js
@@ -0,0 +1,11 @@
+import markermatch from '../images/markermatch.png';
+
+function Navbar() {
+    return (
+      <div className="header">
+        <img src={markermatch} className="logo"></img>
+      </div>
+    );
+  }
+
+export default Navbar
---
+++ markermatch/src/pages/Homepage.js
@@ -0,0 +1,10 @@
+import markermatch from '../images/markermatch.png';
+import Navbar from '../components/Navbar';
+
+function Homepage() {
+    return (
+      <Navbar/> // we can simply import reusable navbar component for each page
+    );
+  }
+
+export default Homepage
