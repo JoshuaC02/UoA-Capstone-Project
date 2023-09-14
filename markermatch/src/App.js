8f76aee7275c6b4b100b6bedc4b22ea06a29de15
Bruno William <blin125@aucklanduni.ac.nz>
2023-09-15 09:50:15 +1200

    just added the student-interface link

--- markermatch/src/App.js
+++ markermatch/src/App.js
@@ -2,24 +2,26 @@ import './styles/HorizontalLayout.css';
 import './styles/NavbarComp.css';
 import './styles/Sidebar.css';
 import Homepage from './pages/Homepage';
+import Studentpage from './pages/Studentpage';
 
 import React, { useState, useEffect } from 'react';
 
 
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import Routes and Route
 
 
 function App() {
   useEffect(() => {
-    // Set body overflow to "hidden" when the component mounts
     document.body.style.overflow = "hidden";
 
   }, []);
     return (
     <>
       <Router>
-          <Homepage/>
-          
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/student-interface" element={<Studentpage/>} />
+          </Routes>
       </Router>
     </>
   );
---
+++ markermatch/src/pages/Studentpage.js
@@ -0,0 +1,11 @@
+import NavbarComp from '../components/NavbarComp';
+import Sidebar from '../components/Sidebar';
+
+function StudentPage() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default StudentPage;
