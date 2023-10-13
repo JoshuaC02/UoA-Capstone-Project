4d09b6c3031348d0e312f979b4a1feda66564baa
Bruno William <blin125@aucklanduni.ac.nz>
2023-10-13 19:57:15 +1300

    fixed the modal pop up

--- markermatch/src/components/NavbarComp.js
+++ markermatch/src/components/NavbarComp.js
@@ -10,42 +10,40 @@ import ModalPopUp from './ModalPopUp';
 function NavbarComp() {
   const { user, signOut } = useAuthenticator((context) => [context.user, context.signOut]);
   const navigate = useNavigate();
-  const [showModal, setShowModal] = useState(true);
-
-  function logOut() {
-    signOut();
-    navigate("/", { replace: true });
-    setShowModal(true);
-  }
+  const [showModal, setShowModal] = useState(false);
 
   function closeModal() {
+    signOut();
+    navigate("/", { replace: true });
     setShowModal(false);
   }
 
   return (
     <div>
       <Navbar expand="sm" className="my-navbar" style={{ padding: '0px' }}>
-        <Navbar.Brand href="#home" className="logo">
-          <img src={markermatch} id="logo" alt="Logo"></img>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse className="my-nav-list" id="basic-navbar-nav">
-          <div className="user login">
-            {user ? (
-              <>
-                <p className="adjust sign">Hello {user.attributes.given_name}!</p>
-              </>
-            ) : (
-              <a href="/auth"><p className="adjust sign">Login</p></a>
-            )}
-            <BiUser className="adjust-user-size" />
-          </div>
-          <div className="user logout">
-            <a style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }} onClick={() => logOut()}><p className="adjust sign">Sign out</p></a>
-                    <PiSignOut className="adjust-user-size"/>
+              <Navbar.Brand href="#home" className="logo">
+                <img src={markermatch} id="logo" alt="Logo"></img>
+              </Navbar.Brand>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse className="my-nav-list" id="basic-navbar-nav">
+                <div className="user login">
+                  {user ? (
+                    <>
+                      <p className="adjust sign">Hello {user.attributes.given_name}!</p>
+                    </>
+                  ) : (
+                    <a href="/auth"><p className="adjust sign">Login</p></a>
+                  )}
+                  <BiUser className="adjust-user-size" />
                 </div>
-        </Navbar.Collapse>
-    </Navbar>
+                <div className="user logout">
+                    <a style={{ textDecoration: 'underline', color: 'blue', cursor: 'pointer' }}onClick={() => setShowModal(true)}>
+                        <p className="adjust sign">Sign out</p>
+                    </a>
+                </div>
+                <PiSignOut className="adjust-user-size" />
+              </Navbar.Collapse>
+      </Navbar>
 
       {showModal && (
         <ModalPopUp
