4649ed8db6fe4df36a17787c8e337b8b58583b5b
Saif3n <nicholaswu498@gmail.com>
2023-10-20 13:14:54 +1300

    formatted marker application form to make it look nicer

--- markermatch/src/components/MarkerApplicationForm.js
+++ markermatch/src/components/MarkerApplicationForm.js
@@ -202,10 +202,23 @@ function MarkerApplicationForm() {
 
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
-        setFormData(prevData => ({
-            ...prevData,
-            [name]: type === 'checkbox' ? checked : value
-        }));
+    
+        if (type === 'radio') {
+            setFormData(prevData => ({
+                ...prevData,
+                [name]: value === "Yes"
+            }));
+        } else if (type === 'checkbox') {
+            setFormData(prevData => ({
+                ...prevData,
+                [name]: checked
+            }));
+        } else {
+            setFormData(prevData => ({
+                ...prevData,
+                [name]: value
+            }));
+        }
     };
 
     const handlePreferenceChange = async (e) => { 
@@ -298,7 +311,7 @@ function MarkerApplicationForm() {
         }        
 
         try {
-
+            console.log(formData)
             await DataStore.save(
                 new MarkerApplication({
                     givenName: formData.givenName,
@@ -453,138 +466,196 @@ function MarkerApplicationForm() {
                 {step === 2 && (
                 <div>
 
-                    <Row className="mb-3">
-                    <Form.Group as={Col}>
-                        <Form.Label>Given Name</Form.Label>
-                        <Form.Control
-                            name="givenName"
-                            placeholder="John"
-                            value={formData.givenName}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+<Row className="justify-content-center mb-3">
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">Given Name</Form.Label>
+        <Form.Control
+            name="givenName"
+            placeholder="John"
+            value={formData.givenName}
+            onChange={handleChange}
+        />
+    </Form.Group>
 
-                    <Form.Group as={Col}>
-                        <Form.Label>Family Name</Form.Label>
-                        <Form.Control
-                            name="familyName"
-                            placeholder="Doe"
-                            value={formData.familyName}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">Family Name</Form.Label>
+        <Form.Control
+            name="familyName"
+            placeholder="Doe"
+            value={formData.familyName}
+            onChange={handleChange}
+        />
+    </Form.Group>
+    </Row>
 
-                    <Form.Group as={Col}>
-                        <Form.Label>Auckland University ID (AUID)</Form.Label>
-                        <Form.Control
-                            name="auid"
-                            placeholder="e.g. 123456789"
-                            value={formData.auid}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+    <Row className="justify-content-center mb-3">
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">Auckland University ID (AUID)</Form.Label>
+        <Form.Control
+            name="auid"
+            placeholder="e.g. 123456789"
+            value={formData.auid}
+            onChange={handleChange}
+        />
+    </Form.Group>
 
-                    <Form.Group as={Col}>
-                        <Form.Label>University Username (UPI)</Form.Label>
-                        <Form.Control
-                            name="upi"
-                            placeholder="e.g. tuoa001"
-                            value={formData.upi}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">University Username (UPI)</Form.Label>
+        <Form.Control
+            name="upi"
+            placeholder="e.g. tuoa001"
+            value={formData.upi}
+            onChange={handleChange}
+        />
+    </Form.Group>
 
-                    <Form.Group as={Col}>
-                        <Form.Label>Contact Email:</Form.Label>
-                        <Form.Control
-                            name="preferredEmail"
-                            value={formData.preferredEmail}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-                </Row>
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">Contact Email:</Form.Label>
+        <Form.Control
+            name="preferredEmail"
+            value={formData.preferredEmail}
+            onChange={handleChange}
+        />
+    </Form.Group>
+</Row>
 
-                <Row className="mb-3">
-                    <Form.Group as={Col}>
-                        <Form.Label>Are you currently overseas?</Form.Label>
-                        <Form.Check
-                            type="checkbox"
-                            id="overseas"
-                            checked={formData.overseas}
-                            onChange={handleChange}
-                            name="overseas"
-                        />
-                    </Form.Group>
+<Row className="justify-content-center mb-3">
 
-                    <Form.Group as={Col}>
-                        <Form.Label>Are you legally allowed to work in New Zealand (Resident or Work Permit)</Form.Label>
-                        <Form.Check
-                            type="checkbox"
-                            id="validNzWorkPermit"
-                            checked={formData.validNzWorkPermit}
-                            onChange={handleChange}
-                            name="validNzWorkPermit"
-                        />
-                    </Form.Group>
 
-                    <Form.Group as={Col}>
-                        <Form.Label>What degree are you studying?</Form.Label>
-                        <Form.Control
-                            name="degree"
-                            placeholder="e.g. Bachelor of Science, Major in Computer Science"
-                            value={formData.degree}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
+    
 
-                    <Form.Group as={Col}>
-                        <Form.Label>How long have you been studying?</Form.Label>
-                        <Form.Control
-                            name="yearsOfStudy"
-                            placeholder="e.g. 2 years"
-                            value={formData.yearsOfStudy}
-                            onChange={handleChange}
-                        />
-                    </Form.Group>
-                </Row>
-                <Row className="mb-3">
-                    <Form.Group as={Col} className="d-flex align-items-center">
-                        <Form.Label>Are you an Undergraduate or Postgraduate student?</Form.Label>
-                        <Form.Select
-                            name="underPostGrad"
-                            aria-label="Default select example"
-                            value={formData.underPostGrad}
-                            onChange={handleChange}
-                        >
-                            <option value="Undergraduate">Undergraduate</option>
-                            <option value="Postgraduate">Postgraduate</option>
-                        </Form.Select>
-                    </Form.Group>
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">What degree are you studying?</Form.Label>
+        <Form.Control
+            name="degree"
+            placeholder="e.g. Bachelor of Science, Major in Computer Science"
+            value={formData.degree}
+            onChange={handleChange}
+        />
+    </Form.Group>
 
-                    <Form.Group as={Col} className="d-flex align-items-center">
-                        <Form.Label>Are you currently contracted as a tutor?</Form.Label>
-                        <Form.Check
-                            type="checkbox"
-                            id="currentTutor"
-                            checked={formData.currentTutor}
-                            onChange={handleChange}
-                            name="currentTutor"
-                        />
-                    </Form.Group>
-                </Row>
+    <Form.Group as={Col}>
+        <Form.Label className="w-100">How long have you been studying?</Form.Label>
+        <Form.Control
+            name="yearsOfStudy"
+            placeholder="e.g. 2 years"
+            value={formData.yearsOfStudy}
+            onChange={handleChange}
+        />
+    </Form.Group>
+</Row>
 
-                <Row className="mb-3">
-                    <Form.Group as={Col} className="d-flex align-items-center">
-                        <Form.Label>How many hours can you work this Semester?</Form.Label>
-                        <Form.Control
-                            name="maxHours"
-                            value={formData.maxHours}
-                            onChange={handleChange}
-                            type="number"
-                        />
-                    </Form.Group>
 
-                </Row>
+<Row className="justify-content-center mb-3">
+
+<Form.Group as={Col} className="d-flex align-items-center">
+        <Form.Label className="w-100">Are you an Undergraduate or Postgraduate student?</Form.Label>
+        <Form.Select
+            name="underPostGrad"
+            aria-label="Default select example"
+            value={formData.underPostGrad}
+            onChange={handleChange}
+        >
+            <option value="Undergraduate">Undergraduate</option>
+            <option value="Postgraduate">Postgraduate</option>
+        </Form.Select>
+    </Form.Group>
+
+    <Form.Group as={Col} className="d-flex align-items-center">
+        <Form.Label className="w-100">What is the maximum number of hours you can work per week?</Form.Label>
+        <Form.Control
+            name="maxHours"
+            value={formData.maxHours}
+            onChange={handleChange}
+            type="number"
+            style={{ width: '100px' }}
+        />
+    </Form.Group>
+</Row>
+
+<Row className="justify-content-center mb-3">
+  <h4>Please select if the following applies to you:</h4>
+</Row>
+
+<Row className="justify-content-center mb-3">
+<Form.Group as={Col} className="d-flex align-items-center border p-3">
+    <Form.Label className="w-100">Are you legally allowed to be employed in NZ?</Form.Label>
+    <div className="d-flex">
+        <Form.Check
+            type="radio"
+            id="validNzWorkPermitYes"
+            name="validNzWorkPermit"
+            value="Yes"
+            checked={formData.validNzWorkPermit}
+            onChange={handleChange}
+            label="Yes"
+            className="mr-3"
+        />
+        <Form.Check
+            type="radio"
+            id="validNzWorkPermitNo"
+            name="validNzWorkPermit"
+            value="No"
+            checked={!formData.validNzWorkPermit}
+            onChange={handleChange}
+            label="No"
+        />
+    </div>
+</Form.Group>
+
+<Form.Group as={Col} className="d-flex align-items-center border p-3">
+    <Form.Label className="w-100">Are you currently contracted as a tutor?</Form.Label>
+    <div className="d-flex">
+        <Form.Check
+            type="radio"
+            id="currentTutorYes"
+            name="currentTutor"
+            value="Yes"
+            checked={formData.currentTutor}
+            onChange={handleChange}
+            label="Yes"
+            className="mr-3"
+        />
+        <Form.Check
+            type="radio"
+            id="currentTutorNo"
+            name="currentTutor"
+            value="No"
+            checked={!formData.currentTutor}
+            onChange={handleChange}
+            label="No"
+        />
+    </div>
+</Form.Group>
+
+<Form.Group as={Col} className="d-flex align-items-center border p-3">
+    <Form.Label className="w-100">Are you currently overseas?</Form.Label>
+    <div className="d-flex">
+        <Form.Check
+            type="radio"
+            id="overseasYes"
+            name="overseas"
+            value="Yes"
+            checked={formData.overseas}
+            onChange={handleChange}
+            label="Yes"
+            className="mr-3"
+        />
+        <Form.Check
+            type="radio"
+            id="overseasNo"
+            name="overseas"
+            value="No"
+            checked={!formData.overseas}
+            onChange={handleChange}
+            label="No"
+        />
+    </div>
+</Form.Group>
+
+</Row>
+
+
                     <button className="previous-button" type="button" onClick={handlePrevious}>Previous</button>
                     <button className="next-button" type="button" onClick={handleNext}>Next</button>
                 </div>
